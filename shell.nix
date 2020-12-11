2f572299336cc0d2fba43a41cd14b53661536b38
Sek Un <sekun@hey.com>
2020-12-11 12:02:36 +0800

    Add nix-shell

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+  
+with pkgs;
+
+let
+  inherit (lib) optional optionals;
+
+  erlang = beam.interpreters.erlangR23;
+  elixir = beam.packages.erlangR23.elixir_1_11;
+  nodejs = nodejs-12_x;
+in
+
+mkShell {
+  buildInputs = [cacert git erlang elixir cargo nodejs postgresql]
+    ++ optional stdenv.isLinux inotify-tools
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]);
+
+    shellHook = ''
+      export LANG=C.UTF-8
+    '';
+}
