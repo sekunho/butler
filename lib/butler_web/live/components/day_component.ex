8513c8910596313c3951c9f4551f900fc006cf6d
Sek Un <sekun@hey.com>
2020-12-12 12:28:44 +0800

    Update schedule template

--- lib/butler_web/live/components/day_component.ex
+++ lib/butler_web/live/components/day_component.ex
@@ -4,25 +4,33 @@ defmodule ButlerWeb.DayComponent do
   def render(assigns) do
     ~L"""
     <div>
-      <div class="day-header">
-        <h5 class="day-header__name"><%= get_day_name(@day) %></h5>
-        <span class="day-header__number <%= if is_today?(@day), do: "day-header__number--active", else: "" %>"><%= get_day_num(@day) %></span>
-      </div>
-      <ul class="mt-4">
-        <%= if is_disabled?(@day) do %>
-          <li class="bg-gray-600 opacity-10 cursor-not-allowed select-none text-white"
-              style="height: calc(3.25rem * 24);">
-          </li>
-        <% else %>
-          <%= for todo <- @todos do %>
-            <li class="absolute bg-indigo-500 px-1 rounded cursor-pointer select-none hover:shadow-md text-white"
-                style="height: calc(3.25rem * <%= todo.duration %>); margin-top: calc(3.25rem * <%= todo.start %>);">
-              <span class="text-xs font-medium">Continue with final project</span>
-              <span class="text-xs">(00:00 - 00:15)</span>
-            </li>
-          <% end %>
-        <% end %>
-      </ul>
+        <div class="day-header">
+            <h5 class="day-header__name"><%= get_day_name(@day) %></h5>
+            <span class="day-header__number <%= if is_today?(@day), do: "day-header__number--active", else: "" %>">
+                <%= get_day_num(@day) %>
+            </span>
+        </div>
+        <ul class="mt-4 relative">
+            <%= if is_disabled?(@day) do %>
+                <li class="bg-gray-400 opacity-10 cursor-not-allowed select-none text-white"
+                    style="height: calc(3.25rem * 24);">
+                </li>
+            <% else %>
+                <%= for todo <- @todos do %>
+                    <li class="absolute w-full bg-indigo-500 px-1 rounded cursor-pointer select-none hover:shadow-lg text-white"
+                        style="height: calc(3.25rem * <%= todo.duration %>); margin-top: calc(3.25rem * <%= todo.start %>);">
+                        <p class="text-xs font-medium truncate">
+                            <span>
+                                Continue with final project
+                            </span>
+                            <span class="ml-1.5">
+                                0:00
+                            </span>
+                        </p>
+                    </li>
+                <% end %>
+            <% end %>
+        </ul>
     </div>
     """
   end
