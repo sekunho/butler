b04e0813b395f5b48ee155998f94d508349fbbf4
Sek Un <sekun@hey.com>
2020-12-12 17:40:23 +0800

    hotfix: Fix ssl config

--- config/prod.exs
+++ config/prod.exs
@@ -47,7 +47,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :butler, ButlerWeb.Endpoint, force_ssl: [hsts: true]
+# config :butler, ButlerWeb.Endpoint, force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
