4a55085d8f63d5fefab1ada432ca618e79831c6d
Sek Un <sekun@hey.com>
2020-12-19 11:48:31 +0000

    Add landing page (#35)

--- lib/butler_web/live/page_live.ex
+++ lib/butler_web/live/page_live.ex
@@ -2,38 +2,15 @@ defmodule ButlerWeb.PageLive do
   use ButlerWeb, :live_view
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
-  end
+  def mount(_params, session, socket) do
+    socket = assign_defaults(socket, session)
 
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+    case socket.assigns.current_user do
+      nil -> {:ok, socket}
 
       _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
+        {:ok,
+          push_redirect(socket, to: Routes.todo_index_path(socket, :index))}
     end
-  end
-
-  defp search(query) do
-    if not ButlerWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
